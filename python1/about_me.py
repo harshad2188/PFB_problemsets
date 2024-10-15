0602cc4a0009ad931d939202c94a4de862f46b69
pfb2024 <pfb2024@info26.cshl.edu>
2024-10-15 12:07:47 -0400

    python1 problemset

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-name= 'harshad'
-print(name)
-myfavcolor='Blue'
-print (myfavcolor)
+import sys
+name= 'Harshad'
+myfavcolor='My favorite color: Blue'
+myfavactivity='My favorite activity: Running'
+myfavanimal='My favorite animal : dog'
+print (myfavanimal)
+print (name, myfavcolor, myfavactivity)
+print(f"My name: {name}")
+
+friend1= sys.argv[1]
+friend2=sys.argv[2]
+print(friend1, 'and' ,friend2, 'are friends')
 
 
 
